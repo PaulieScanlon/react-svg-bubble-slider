932623decbf6b0e11816e7537fc37dbbabc35270
pauliescanlon <pauliescanlon@gmail.com>
2020-05-15 17:18:04 +0100

    convert to TypeScript - add Webpack build script

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  mode: "production",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+    ],
+  },
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+};

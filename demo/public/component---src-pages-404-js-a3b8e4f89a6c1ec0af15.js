fef59c69f5dee2d45befdca335823f0ebadb85e5
pauliescanlon <pauliescanlon@gmail.com>
2020-06-11 15:15:12 +0100

    remove demo/public

--- demo/public/component---src-pages-404-js-a3b8e4f89a6c1ec0af15.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{w2l6:function(n,e,t){"use strict";t.r(e);var o=t("mXGw"),u=t.n(o);e.default=function(){return u.a.createElement("div",null,"Page Not Found")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-a3b8e4f89a6c1ec0af15.js.map
